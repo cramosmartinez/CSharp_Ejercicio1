955db770a6422ec046f130cd39a30b564ea61854
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 17:38:05 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Crear un programa que sume 3 números enteros y muestre el resultado en pantalla.
+var numero = 12;
+var numero2 = 23;
+var numero3 = 36;
+var resultado = numero + numero2 + numero3;
+Console.WriteLine(resultado);
